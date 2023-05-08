91d24317911149065c80100a762b6228da7c26a4
shahab <ashahab2727@gmail.com>
2023-05-08 18:38:08 +0330

    add comment class

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.conf import settings
-from django.contrib.auth.models import User
 from django.urls import reverse
 
 
@@ -14,7 +13,7 @@ class Post(models.Model):
     
     
     def __str__(self):
-        return self.title
+        return f'{self.title} - {self.content[:50]}'
     
     
     class Meta:
@@ -22,8 +21,14 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return reverse('post:post_detail', kwargs={'slug': self.slug})
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
+    reply = models.ForeignKey("self", on_delete=models.CASCADE, related_name='reply', blank=True, null=True)
+    content = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
     
     
-    
-    
-    
+    def __str__(self):
+        return f'{self.user} - {self.content[:20]}'
